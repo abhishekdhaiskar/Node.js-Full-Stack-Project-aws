ad09308b22883f66458820570518712dea2fc6d9
abhishekdhaiskar <120624413+abhishekdhaiskar@users.noreply.github.com>
2025-06-20 19:12:16 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,15 @@
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
 
-output "cluster_endpoint" { value = module.eks.cluster_endpoint }
-output "cluster_ca_certificate" { value = module.eks.cluster_certificate_authority_data }
-output "db_address" { value = aws_db_instance.postgres.address }
-output "db_port" { value = aws_db_instance.postgres.port }
+output "cluster_ca_certificate" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "db_address" {
+  value = aws_db_instance.postgres.address
+}
+
+output "db_port" {
+  value = aws_db_instance.postgres.port
+}
