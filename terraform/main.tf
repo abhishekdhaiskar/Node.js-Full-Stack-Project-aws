4e105694f3771e56134b709b600ebdf9a8fdeddb
abhishekdhaiskar <120624413+abhishekdhaiskar@users.noreply.github.com>
2025-06-20 19:11:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.35.0"  # Not v6.0.0!
+    }
+  }
+}
+
 
 provider "aws" {
   region = var.aws_region
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "fullstack-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db-sg"
+  description = "Allow DB access"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.29.0"
+
   cluster_name    = "fullstack-eks"
-  vpc_id          = var.vpc_id
-  subnet_ids      = var.subnet_ids
-  node_groups = {
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
     main = {
       desired_capacity = 2
       instance_type    = "t3.medium"
@@ -19,16 +65,17 @@ module "eks" {
 
 resource "aws_db_subnet_group" "dbsubnet" {
   name       = "fullstack-db-subnet"
-  subnet_ids = var.subnet_ids
+  subnet_ids = module.vpc.private_subnets
 }
 
 resource "aws_db_instance" "postgres" {
-  identifier         = "fullstack-db"
-  engine             = "postgres"
-  instance_class     = "db.t3.micro"
-  username           = var.db_master_username
-  password           = var.db_master_password
-  allocated_storage  = 20
-  db_subnet_group_name = aws_db_subnet_group.dbsubnet.name
-  skip_final_snapshot   = true
+  identifier             = "fullstack-db"
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  username               = var.db_master_username
+  password               = var.db_master_password
+  allocated_storage      = 20
+  db_subnet_group_name   = aws_db_subnet_group.dbsubnet.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  skip_final_snapshot    = true
 }
